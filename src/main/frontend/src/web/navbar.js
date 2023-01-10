edd843066e7e43f28cf960dbb137c45257efbb49
HyunJoonS <81973827+HyunJoonS@users.noreply.github.com>
2023-01-10 20:08:56 +0900

    css수정

--- src/main/frontend/src/web/navbar.js
+++ src/main/frontend/src/web/navbar.js
@@ -3,18 +3,26 @@ import { useNavigate } from 'react-router-dom';
 import { FaRegCommentDots } from "react-icons/fa";
 import Chat from './Chat';
 import { useState } from 'react';
+import axios from 'axios';
 function MyNavbar() {
   let nevigate = useNavigate();
   let [visibleChat,setVisibleChat] = useState(false);
   const openChat = ()=>{
     setVisibleChat(!visibleChat);
   }
+  const goAdminPage =()=>{
+    axios.get('api/admin').then(()=>{
+        nevigate('/adminpage');
+    }).catch(({response})=>{
+        nevigate("/login");
+    })
+  } 
   return (
     <div className={styles.gnb}>
       <div className={styles.wrap}>
         <a className={styles.logo} onClick={() => { nevigate('/') }}>BOOKCAFE</a>
         <a className={styles.link} onClick={() => { nevigate('/') }}>UserHome</a>
-        <a className={styles.link} onClick={() => { nevigate('/adminpage') }}>AdminPage</a>
+        <a className={styles.link} onClick={goAdminPage}>AdminPage</a>
         <div className={styles.chat}>
           <a className={styles.chatBtn} onClick={openChat}>
             <FaRegCommentDots />
